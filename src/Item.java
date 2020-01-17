e6fb51bc627aa92e1bcb0e2bb407d051338eea32
suminhong02 <sumin.hong@ssfs.org>
2020-01-17 09:51:24 -0500

    Completed Item

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,24 @@
 public class Item {
+    private String number;
+    private String description;
+    private double price;
+    public Item(String n, String d, double p){
+        number = n;
+        description = d;
+        price = p;
+        if(price<0){
+            price = 0;
+        }
+    }
+    public double getTotalPrice(int quantity){
+        if(quantity<0){
+            quantity = 0;
+        }
+        double total = price*quantity;
+        return total;
+    }
+
+    public String toString(){
+        return "Item: "+number+"\nPrice: $"+price+"\nDescription: "+description;
+    }
 }
