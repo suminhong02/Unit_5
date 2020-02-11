eb3a1586673d99c8244618a203be6a9032fdd63a
suminhong02 <hongsu@ssfs.org>
2020-02-10 21:07:45 -0500

    Working on Fraction

---
+++ src/Fraction.java
@@ -0,0 +1,19 @@
+/**
+ * This class models fraction
+ * @author Sumin Hong
+ * @since February 10, 2020
+ */
+public class Fraction {
+    private int numerator;
+    private int denominator;
+    /**
+     * First constructor
+     * @param numerator the numerator of fraction
+     * @param denominator the denominator of fraction
+     */
+
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+}
